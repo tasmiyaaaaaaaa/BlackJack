a8ccf4e5369473eed9d985055b3eed2cd39f560c
tasmiyaa <tassumi07@gmail.com>
2025-08-12 22:10:33 +0300

    added the deck class

--- blackjack.py
+++ blackjack.py
@@ -5,14 +5,34 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
             'Queen':10, 'King':10, 'Ace':11}
 
+playing = True
+
 class Card():
     def __init__(self,rank,suit):
         self.rank = rank
         self.suit = suit
-        self.card = values[rank]
 
     def __str__(self):
-        return f"{self.card} of {self.suit}"
+        return f"{self.rank} of {self.suit}"
 
-two_of_hearts = Card(ranks[10], suits[3])
-print(two_of_hearts)
+class Deck():
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(rank,suit))
+    
+    def __str__(self):
+        every_card = ''
+        for card in self.deck:
+            every_card+= '\n' + str(card)
+        return f'THE DECK HAS: {every_card}'
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+    
+
